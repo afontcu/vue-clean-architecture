895dbfbc641bb6593abcef3481ea4fa8658209f8
Adrià Fontcuberta <afontcu@gmail.com>
2019-03-25 07:35:49 +0100

    Register base store

---
+++ web/pages/contracts/index.vue
@@ -0,0 +1,21 @@
+<template>
+  <div></div>
+</template>
+
+<script>
+import get from 'lodash/get'
+
+import store from './store'
+
+export default {
+  name: 'Contracts',
+
+  beforeCreate() {
+    if (!get(this.$store, 'state.contracts')) {
+      this.$store.registerModule('contracts', store)
+    }
+  },
+}
+</script>
+
+<style scoped></style>
---
+++ web/pages/contracts/store/index.js
@@ -0,0 +1,12 @@
+import getters from './getters'
+import mutations from './mutations'
+import actions from './actions'
+
+const state = []
+
+export default {
+  state,
+  getters,
+  mutations,
+  actions,
+}
